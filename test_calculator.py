0f5f1b16d70a14ebbd96c0d62f13f061f247643f
JetLiTheQT <ekkachai.ittihrit2@gmail.com>
2021-06-06 22:29:18 -0700

    Fixed W293 white space issue

--- test_calculator.py
+++ test_calculator.py
@@ -6,10 +6,10 @@ import calculator
 
 class TestCalculatorApp:
     def test_add(self):
-        assert 5 == calculator.add(3, 2)
-    
+        assert 5 == calculator.add(3, 2)   
+
     def test_difference(self):
         assert 5 == calculator.difference(10, 5)
-
+     
     def test_multiple(self):
         assert 4 == calculator.multiply(2, 2)
